82ce00307ef4f05c95a73ca26d258d9901ccf0ec
DapCodes <daffaramadhan929@gmail.com>
2025-02-01 13:11:59 +0700

    Matrix Multiplication

---
+++ script.js
@@ -0,0 +1,33 @@
+function multiplyMatrices() {
+  const matrixA = [];
+  const matrixB = [];
+  const resultMatrix = [];
+
+  // A
+  for (let i = 0; i < 3; i++) {
+    matrixA[i] = [];
+    for (let j = 0; j < 3; j++) {
+      matrixA[i][j] = Number(document.getElementById(`a${i}${j}`).value) || 0;
+    }
+  }
+
+  // B
+  for (let i = 0; i < 3; i++) {
+    matrixB[i] = [];
+    for (let j = 0; j < 3; j++) {
+      matrixB[i][j] = Number(document.getElementById(`b${i}${j}`).value) || 0;
+    }
+  }
+
+  // C
+  for (let i = 0; i < 3; i++) {
+    resultMatrix[i] = [];
+    for (let j = 0; j < 3; j++) {
+      resultMatrix[i][j] = 0;
+      for (let k = 0; k < 3; k++) {
+        resultMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+      }
+      document.getElementById(`r${i}${j}`).textContent = resultMatrix[i][j];
+    }
+  }
+}
